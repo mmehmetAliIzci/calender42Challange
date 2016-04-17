0c4925fc48d3b99c965f9b4ce9c8d08a097baeba
mmehmetAliIzci <mmehmetaliizci@hotmail.com>
2016-04-17 17:27:47 +0300

    Commit Before Moment js

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -1,12 +1,46 @@
 import {Component} from 'angular2/core';
 
+
 @Component({
     selector: 'my-app',
-    template: `
-        <h1>Angular 2 Boilerplate</h1>
-        <p>Hello World!</p>
-    `,
+    templateUrl: 'dev/app.html'
+
 })
 export class AppComponent {
+  seconds=0;
+  minutes=0;
+  hours=0;
+  timerTokens: Array<number> = [];
+  items: Array<number> = [this.hours,this.minutes,this.seconds];
+  constructor(){}
 
+  startCounting(){
+    this.timerTokens.push(setInterval(() =>{
+      (this.items[2]++)%10;
+    }, 1000));
+    this.timerTokens.push(setInterval(() =>{
+      (this.items[1]++)%60;
+    }, 60000));
+    this.timerTokens.push(setInterval(() =>{
+      (this.items[0]++)%99;
+    }, 360000));
+  }
+  stopCounting(){
+    this.clearTimeTokens();
+  }
+  resetCounting(){
+    this.clearTimeTokens();
+    this.clearAll();
+  }
+
+  clearAll(){
+    for (let i = 0; i < this.items.length; i++) {
+        this.items[i]=0;
+    }
+  }
+  clearTimeTokens(){
+    for (let i = 0; i < this.timerTokens.length; i++) {
+        clearTimeout(this.timerTokens[i]);
+    }
+  }
 }
---
+++ dev/services/calender.service.ts
@@ -0,0 +1,51 @@
+import {Inject,Injectable} from 'angular2/core';
+import {Observable} from 'rxjs/Observable';
+import {Observer} from 'rxjs/Observer';
+import {Http, Headers} from 'angular2/http';
+import 'rxjs/add/observable/fromArray';
+import 'rxjs/add/operator/concatMap';
+import 'rxjs/add/operator/share';
+import 'rxjs/add/operator/map';
+
+const URL: string = 'https://demo.calendar42.com/api/v2/events/';
+const TOKEN: string = '3c8da1482ffeea7a9c14f20cc512a8c83b805a0c';
+
+@Injectable()
+export class CalenderService  {
+
+    constructor(public http: Http) {
+    }
+
+    postTime(start_time,end_time) {
+
+
+      return new Promise(resolve => {
+
+        let headers = new Headers();
+        headers.append('Accept', 'application/json');
+        headers.append('Content-type', 'application/json');
+        headers.append('Accept', 'Token '+ TOKEN);
+
+        let PAYLOAD = {
+                      event_type: "normal",
+                      title: "My Tracked time",
+                      start: start_time,
+                      start_timezone: "",
+                      end: end_time,
+                      end_timezone: "",
+                      rsvp_status: "attending"
+                  };
+        let data = JSON.stringify(PAYLOAD);
+
+
+       this.http.post(URL,data,{headers: headers})
+       .map(res => res.json())
+       .subscribe((response) => {
+           // we've got back the raw data, now generate the core schedule data
+           // and save the data for later reference
+           resolve(response);
+        });
+     });
+   }
+
+}
